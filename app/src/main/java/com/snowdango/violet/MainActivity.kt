048d2bb59530cb13e47598aac7af78cd1c10cfa8
SnowDango <xdi.grand.you.winner@gmail.com>
2022-11-20 05:09:00 +0900

    色々

--- app/src/main/java/com/snowdango/violet/MainActivity.kt
+++ app/src/main/java/com/snowdango/violet/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.snowdango.violet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            // TODO create navHost
+        }
     }
 
 }
