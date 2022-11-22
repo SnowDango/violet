55adbbc506ddbef70919f967b2ad93bcd3a992a3
SnowDango <xdi.grand.you.winner@gmail.com>
2022-11-22 19:12:38 +0900

    色々

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,10 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class:1.1.0-alpha02")
     implementation("androidx.navigation:navigation-compose:2.5.3")
 
+    // room
+    implementation("androidx.room:room-runtime:2.4.3")
+    kapt("androidx.room:room-compiler:2.4.3")
+
     // compose lifecycle
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
