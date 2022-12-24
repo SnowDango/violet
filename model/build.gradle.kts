d2aaf5a41028222e2206b856e19706fc28331e23
SnowDango <xdi.grand.you.winner@gmail.com>
2022-12-25 04:28:13 +0900

    add model

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,44 +1,45 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id(plugs.plugins.library.get().pluginId)
+    id(plugs.plugins.kotlinAndroid.get().pluginId)
 }
 
 android {
     namespace = "com.snowdango.violet.model"
-    compileSdk = 32
+    compileSdk = vers.versions.sdk.get().toInt()
 
     defaultConfig {
-        minSdk = 28
-        targetSdk = 32
+        minSdk = vers.versions.minsdk.get().toInt()
+        targetSdk = vers.versions.sdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
+
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+
+        create("staging") {
+            isMinifyEnabled = true
+        }
+
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+
 }
