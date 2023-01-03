1c0bd5c2bd4976784dfbdc2609232714e1ce6c54
SnowDango <xdi.grand.you.winner@gmail.com>
2023-01-04 07:53:53 +0900

    add pull to refreshing

--- app/src/main/java/com/snowdango/violet/viewmodel/history/HistoryViewModel.kt
+++ app/src/main/java/com/snowdango/violet/viewmodel/history/HistoryViewModel.kt
@@ -6,12 +6,14 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import com.snowdango.violet.model.SongHistoryModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class HistoryViewModel : ViewModel(), KoinComponent {
 
     private val songHistoryModel: SongHistoryModel by inject()
+    val refreshing = MutableStateFlow(false)
 
     val songHistoryFlow = Pager(
         PagingConfig(pageSize = 100, initialLoadSize = 100)
