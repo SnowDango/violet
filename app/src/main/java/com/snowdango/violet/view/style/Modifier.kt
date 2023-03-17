16290ae07c6e8a493959152aa58da0c1e6f01fac
SnowDango <xdi.grand.you.winner@gmail.com>
2023-03-18 03:54:40 +0900

    Color Showkaseの実装

--- app/src/main/java/com/snowdango/violet/view/style/Modifier.kt
+++ app/src/main/java/com/snowdango/violet/view/style/Modifier.kt
@@ -1,11 +1,13 @@
 package com.snowdango.violet.view.style
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 
+@SuppressLint("UnnecessaryComposedModifier")
 fun Modifier.squareConformFillMaxWidth(fraction: Float): Modifier = composed {
     this.fillMaxWidth(fraction).wrapContentHeight().aspectRatio(1f)
 }
